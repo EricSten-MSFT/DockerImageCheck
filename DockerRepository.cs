468771c3680c271ef083f713a79df5e4566cc46e
Eric Stenson <ericsten@php.net>
2021-06-27 17:21:50 -0700

    First check in

---
+++ DockerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DockerImageCheck
+{
+    class DockerRepository
+    {
+        [Newtonsoft.Json.JsonProperty("Repositories", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public Dictionary<string, Dictionary<string, string>> Repositories { get; set; }
+    }
+}
